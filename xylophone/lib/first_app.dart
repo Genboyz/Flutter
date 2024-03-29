d07bf4a0f8a7b19622e4bd2a79b81dd8814233a7
Olimars <olimarstamirayehu4@gmail.com>
2024-03-29 10:24:57 +0430

    demo xylophone game

---
+++ xylophone/lib/first_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'xyloGame.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const XyloGame();
+  }
+}
