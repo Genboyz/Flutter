a30d2f30f6ea2c39d2c862c4fcc78c6d855253a1
Olimars <olimarstamirayehu4@gmail.com>
2024-04-14 22:33:25 +0300

    update

--- Weather_app/lib/screen/location_screen.dart
+++ Weather_app/lib/screen/location_screen.dart
@@ -28,14 +28,14 @@ class _LocationScreenState extends State<LocationScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  MaterialButton(
+                  ElevatedButton(
                     onPressed: () {},
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
                     ),
                   ),
-                  MaterialButton(
+                  ElevatedButton(
                     onPressed: () {},
                     child: Icon(
                       Icons.location_city,
