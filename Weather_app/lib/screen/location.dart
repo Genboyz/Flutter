4a243c573e5e45b27bb6e33ff4967d8b8c9c4d2d
Genboyz <125403207+Genboyz@users.noreply.github.com>
2024-04-20 11:58:58 +0300

    Update location.dart

--- Weather_app/lib/screen/location.dart
+++ Weather_app/lib/screen/location.dart
@@ -2,8 +2,15 @@ class Location{
   double latitude;
   double longtiude;
   void getCurrentLocation(){
-    print("lat: " + latitude);
-    print("long: "+ longitude);
+     try{
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.lowest);
+    print(position);}
+    catch(e){
+      print(e);
+    }
+    print("lat: " + latitude ?? 20);
+    print("long: "+ longitude ?? 20);
   }
   
 }
