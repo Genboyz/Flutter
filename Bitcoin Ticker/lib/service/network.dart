6be43932aa2bf5eb05ba1730e65dcc5b35a4b0bb
Olimars <125403207+Genboyz@users.noreply.github.com>
2024-05-02 15:01:40 +0300

    Update network.dart

--- Bitcoin Ticker/lib/service/network.dart
+++ Bitcoin Ticker/lib/service/network.dart
@@ -1,1 +1,19 @@
-const String apikey = "2E00BDC9-6DA9-4695-B1CC-25D48125423A";
+import 'package:http/http.dart';
+import 'dart:convert';
+
+const apiKey = "2E00BDC9-6DA9-4695-B1CC-25D48125423A";
+class NetworkHelp {
+  final String url = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey= $apiKey';
+
+  Future getData() async {
+    Response response = await get(
+      Uri.parse(url),
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
