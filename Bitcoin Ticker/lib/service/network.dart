35c860b7dc99f8b59c659d34cdff5c40d778b31a
Olimars <125403207+Genboyz@users.noreply.github.com>
2024-05-04 23:57:30 +0300

    Update network.dart

--- Bitcoin Ticker/lib/service/network.dart
+++ Bitcoin Ticker/lib/service/network.dart
@@ -4,11 +4,14 @@ import 'dart:convert';
 const apiKey = "2E00BDC9-6DA9-4695-B1CC-25D48125423A";
 
 class NetworkHelp {
-  final String url =
-      'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey= $apiKey';
+  String currency;
+  NetworkHelp({required this.currency});
+  
 
   Future getRate() async {
-    Response response = await get(
+   final String url =
+      'https://rest.coinapi.io/v1/exchangerate/BTC/$currency?apikey= $apiKey'; 
+      Response response = await get(
       Uri.parse(url),
     );
     if (response.statusCode == 200) {
