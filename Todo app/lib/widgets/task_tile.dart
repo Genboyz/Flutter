0d9c280fa5bc3f1e53cf42cdd51b7fb84c4c58fe
Olimars <125403207+Genboyz@users.noreply.github.com>
2024-07-24 21:33:15 +0300

    Create task_tile.dart

---
+++ Todo app/lib/widgets/task_tile.dart
@@ -0,0 +1,14 @@
+class TaskTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('it\'s me'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {},
+      ),
+      dense: true,
+      minVerticalPadding: MediaQuery.of(context).size.width * 0.0,
+    );
+  }
+}
